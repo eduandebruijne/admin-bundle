83deaa263b152daef6faa2f735e8422128e2eaed
Eduan de Bruijne <eduan.de.bruijne@gmail.com>
2021-11-26 09:38:10 +0100

    Rename all.js to dist.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/Resources/assets/app.js',
     output: {
-        filename: 'all.js',
+        filename: 'dist.js',
         path: path.resolve(__dirname, 'src/Resources/public'),
     },
     plugins: [
