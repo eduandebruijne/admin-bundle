72f06ffd67c82d4e1be9d905bcaf2bbc1b869c19
Eduan de Bruijne <eduan.de.bruijne@gmail.com>
2021-11-26 09:27:29 +0100

    Add bootstrap JS as global var

--- src/Resources/assets/app.js
+++ src/Resources/assets/app.js
@@ -1,12 +1,14 @@
 import $ from 'jquery'
 import quill from 'quill'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 import {confirmUrl, showAlert, selectMaskedForm} from './js/script';
 
 import 'quill/dist/quill.snow.css'
 import './scss/style.scss'
 
 globalThis.confirmUrl = confirmUrl
+globalThis.jquery = $
+globalThis.bootstrap = bootstrap
+globalThis.Quill = quill
 globalThis.selectMaskedForm = selectMaskedForm
 globalThis.showAlert = showAlert
-globalThis.jquery = $
-globalThis.Quill = quill
