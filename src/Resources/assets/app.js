a1b7dee45feda3b6b9c83c410592403c842b9bbf
Eduan de Bruijne <eduan.de.bruijne@gmail.com>
2021-11-27 11:45:33 +0100

    Include tinymce silver theme in dist.js

--- src/Resources/assets/app.js
+++ src/Resources/assets/app.js
@@ -3,6 +3,7 @@ import {confirmUrl, showAlert, selectMaskedForm} from './js/script';
 import $ from 'jquery'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 import tinymce from 'tinymce'
+import 'tinymce/themes/silver'
 
 globalThis.bootstrap = bootstrap
 globalThis.confirmUrl = confirmUrl
