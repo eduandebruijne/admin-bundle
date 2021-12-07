e227537bf6c314e8bc4c95912b13bc950f5fd627
Eduan de Bruijne <eduan.de.bruijne@gmail.com>
2021-12-07 22:04:04 +0100

    Make Media entity extendable

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -3,92 +3,10 @@
 namespace EDB\AdminBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity
  */
-class Media extends BaseEntity
+class Media extends AbstractMedia
 {
-    /**
-     * @ORM\Column
-     */
-    private ?string $title;
-
-    /**
-     * @ORM\Column
-     */
-    private ?string $filename;
-
-    /**
-     * @ORM\Column(nullable=true)
-     */
-    private ?string $mimeType;
-
-    /**
-     * @ORM\Column(nullable=true)
-     */
-    private ?string $extension;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private ?int $size;
-
-    public ?UploadedFile $update;
-
-    public function __toString()
-    {
-        return $this->title ?? '#';
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title)
-    {
-        $this->title = $title;
-    }
-
-    public function getFilename(): ?string
-    {
-        return $this->filename;
-    }
-
-    public function setFilename(?string $filename): void
-    {
-        $this->filename = $filename;
-    }
-
-    public function getMimeType(): ?string
-    {
-        return $this->mimeType;
-    }
-
-    public function setMimeType(?string $mimeType): void
-    {
-        $this->mimeType = $mimeType;
-    }
-
-    public function getExtension(): ?string
-    {
-        return $this->extension;
-    }
-
-    public function setExtension(?string $extension): void
-    {
-        $this->extension = $extension;
-    }
-
-    public function getSize(): ?int
-    {
-        return $this->size;
-    }
-
-    public function setSize(?int $size): void
-    {
-        $this->size = $size;
-    }
 }
