a7c5d917e9a5e401b0303678eaf6ae261da7bb13
Eduan de Bruijne <eduan.debruijne@mobiliteitsfabriek.nl>
2022-07-11 22:15:36 +0200

    Extend bundle configuration and extension

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EDB\AdminBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('edb-admin');
+
+        $treeBuilder->getRootNode()
+            ->children()
+            ->arrayNode('twitter')
+            ->children()
+            ->integerNode('client_id')->end()
+            ->scalarNode('client_secret')->end()
+            ->end()
+            ->end() // twitter
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/DependencyInjection/EDBAdminExtension.php
+++ src/DependencyInjection/EDBAdminExtension.php
@@ -2,8 +2,12 @@
 
 namespace EDB\AdminBundle\DependencyInjection;
 
+use EDB\AdminBundle\Admin\AdminInterface;
+use EDB\AdminBundle\MenuBuilder\MenuItemInterface;
+use EDB\AdminBundle\Route\Loader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
@@ -24,7 +28,29 @@ class EDBAdminExtension extends Extension implements PrependExtensionInterface
 
         $twig = $container->getExtensionConfig('twig')[0];
         $twig['globals']['media_path'] = $container->getParameter('media_path');
-        
+        $twig['form_themes'] = array_merge($twig['form_themes'], [
+            'bootstrap_5_layout.html.twig',
+            '@EDBAdmin/form/theme.html.twig'
+        ]);
         $container->prependExtensionConfig('twig', $twig);
+
+        $loaderDefinition = new Definition(Loader::class);
+        $loaderDefinition->setTags(['routing.loader']);
+        $container->setDefinition(Loader::class, $loaderDefinition);
+
+        $servicesByInstanceOf = $container->getAutoconfiguredInstanceof();
+
+        if (key_exists(AdminInterface::class, $servicesByInstanceOf)) {
+            # TODO: Check if this works, if it does, tag these services
+            $h=1;
+        }
+
+        if (key_exists(MenuItemInterface::class, $servicesByInstanceOf)) {
+            # TODO: Check if this works, if it does, tag these services
+            $h=1;
+        }
+
+        $adminServices = $container->findTaggedServiceIds('edb.admin');
+        $adminMenuItemServices = $container->findTaggedServiceIds('edb.menu_item');
     }
 }
