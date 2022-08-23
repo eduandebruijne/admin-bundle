7e40bd74fd8c18ee0b67dd613aab6e4184f7d7c5
Eduan de Bruijne <eduan.debruijne@mobiliteitsfabriek.nl>
2022-08-23 21:01:09 +0200

    Restyle media update form theme

--- webpack.sh
+++ webpack.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+docker run --rm -it -v "$(pwd):/app" -w "/app" node:12-alpine yarn install
 docker run -it -v "$PWD":/usr/src/app -w /usr/src/app node:12 node_modules/.bin/webpack-cli
